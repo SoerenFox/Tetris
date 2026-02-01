51c9446ee6184e299d02f709558cecf29d4a9f31
Sören Fox <soeren.0211@gmail.com>
2026-02-01 16:39:04 +0100

    Defin colors for Shapes

---
+++ Core/Inc/Colors.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <raylib.h>
+
+// I – Cyan
+#define BODY_I        Color{   0, 255, 255, 255 }   // Cyan
+#define RIM_I    Color{   0, 178, 178, 255 }   // Dark Cyan
+
+// J – Blue
+#define BODY_J        Color{   0,   0, 255, 255 }   // Blue
+#define RIM_J    Color{   0,   0, 178, 255 }   // Dark Blue
+
+// L – Orange
+#define BODY_L        Color{ 255, 165,   0, 255 }   // Orange
+#define RIM_L    Color{ 178, 115,   0, 255 }   // Dark Orange
+
+// O – Yellow
+#define BODY_O        Color{ 255, 255,   0, 255 }   // Yellow
+#define RIM_O    Color{ 178, 178,   0, 255 }   // Dark Yellow
+
+// S – Green
+#define BODY_S        Color{   0, 255,   0, 255 }   // Green
+#define RIM_S    Color{   0, 178,   0, 255 }   // Dark Green
+
+// T – Purple
+#define BODY_T        Color{ 160,   0, 240, 255 }   // Purple
+#define RIM_T    Color{ 112,   0, 168, 255 }   // Dark Purple
+
+// Z – Red
+#define BODY_Z        Color{ 255,   0,   0, 255 }   // Red
+#define RIM_Z    Color{ 178,   0,   0, 255 }   // Dark Red
